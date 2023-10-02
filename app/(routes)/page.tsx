5418357e78aaa54b851770644559d1b224b1535c
Op-Pautu <pautuop@gmail.com>
2023-10-02 21:41:03 +0530

    Add types for product, size, color, etc and add billboard to home

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,20 @@
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/Billboard";
+import Container from "@/components/ui/Container";
 import React from "react";
 
-const HomePage = () => {
-  return <div>Hello Store</div>;
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("3e7a6909-9242-4a4b-ae0d-7a4b941a3c18");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
