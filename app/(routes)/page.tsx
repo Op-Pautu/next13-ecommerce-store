3307ae641608cc8c22ffbad4adb90dd5e7dadd35
Op-Pautu <pautuop@gmail.com>
2023-10-02 23:34:08 +0530

    Render ProductCard in ProductList

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,17 +1,24 @@
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/Billboard";
+import ProductList from "@/components/ProductList";
 import Container from "@/components/ui/Container";
 import React from "react";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
   const billboard = await getBillboard("3e7a6909-9242-4a4b-ae0d-7a4b941a3c18");
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
   );
