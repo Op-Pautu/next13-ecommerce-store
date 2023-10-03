f808549df2bfe8b4b974188f639673bb7fca6488
Op-Pautu <pautuop@gmail.com>
2023-10-03 13:49:45 +0530

    Add mobile filter UI

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -8,6 +8,7 @@ import React from "react";
 import Filter from "./components/Filter";
 import NoResults from "@/components/ui/NoResults";
 import ProductCard from "@/components/ui/ProductCard";
+import MobileFilter from "./components/MobileFilter";
 
 export const revalidate = 0;
 
@@ -41,7 +42,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
         <Billboard data={category.billboard} />
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
-            {/* Add Mobile Filters */}
+            <MobileFilter sizes={sizes} colors={colors} />
             <div className="hidden lg:block">
               <Filter valueKey="sizeId" name="Sizes" data={sizes} />
               <Filter valueKey="colorId" name="Colors" data={colors} />
