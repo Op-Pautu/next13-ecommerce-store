e8b64b171da35fff8ae789b60c78f1a1f00ab89b
Op-Pautu <pautuop@gmail.com>
2023-10-03 11:51:36 +0530

    Make actions for gettings sizes and colors

---
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -0,0 +1,28 @@
+import getProducts from "@/actions/get-products";
+import React from "react";
+
+export const revalidate = 0;
+
+interface CategoryPageProps {
+  params: {
+    categoryId: string;
+  };
+  searchParams: {
+    colorId: string;
+    sizeId: string;
+  };
+}
+
+const CategoryPage: React.FC<CategoryPageProps> = async ({
+  params,
+  searchParams,
+}) => {
+  const products = await getProducts({
+    categoryId: params.categoryId,
+    colorId: searchParams.colorId,
+    sizeId: searchParams.sizeId,
+  });
+  return <div>category</div>;
+};
+
+export default CategoryPage;
