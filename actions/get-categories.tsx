4e68fda5258a31b6fd7bbd3d9e8270d21a696f12
Op-Pautu <pautuop@gmail.com>
2024-02-22 14:54:08 +0530

    Refactor

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -3,8 +3,19 @@ import { Category } from "@/types";
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
 const getCategories = async (): Promise<Category[]> => {
-  const res = await fetch(URL);
-  return res.json();
+  try {
+    const res = await fetch(URL);
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch categories. Status: ${res.status}`);
+    }
+
+    return res.json();
+  } catch (error: any) {
+    console.error("Error fetching categories:", error.message);
+    // You might want to throw the error here or return a default value
+    throw error;
+  }
 };
 
 export default getCategories;
